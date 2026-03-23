fe962a0563f2c2f44acdd7b87011f7333b720c59
Vinn78 <vinayakgoyal7020@gmail.com>
2026-03-24 01:31:18 +0530

    fix firestore database id

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,10 @@ import { getMessaging } from 'firebase/messaging';
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore(
+  app,
+  "ai-studio-090a78f7-e2cc-4415-bfce-3c847ffcd9b9"
+);
 export const auth = getAuth(app);
 export const messaging = getMessaging(app);
 
