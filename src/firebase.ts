7d81b74bbf2b3c21f6d8e12488c2ad26f17adf5b
Vinn78 <vinayakgoyal7020@gmail.com>
2026-03-24 01:03:46 +0530

    fixed firestore connection

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,7 @@ import { getMessaging } from 'firebase/messaging';
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const messaging = getMessaging(app);
 
